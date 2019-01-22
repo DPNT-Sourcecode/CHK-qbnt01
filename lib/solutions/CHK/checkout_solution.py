d6cd10180ca0ace60ed585a503b78f90acb8d5af
sbx_user1059 <sbx_user1059@ip-10-129-102-97.eu-west-2.compute.internal>
2019-01-22 14:32:47 +0000

    Tue Jan 22 14:32:47 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -10,7 +10,10 @@ def load_prices():
     """
     with open('prices.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
-        prices = {row[0] : {"price": row[1], "deal": row[2]} for row in csv_reader}
+        prices = {
+            row[0]: {"price": row[1], "deal": row[2]}
+            for row in csv_reader
+        }
 
     return prices
 
@@ -23,10 +26,18 @@ def checkout(skus):
     Args:
         skus (string) - the SKUs of all the products in the basket
     Returns:
-        Integer representing the total checkout value of the items 
+        Integer representing the total checkout value of the items
     """
+    total_cost = 0
     prices = load_prices()
-    for sku in skus.split(",")
-    
+    for sku in skus.split(","):
+        if sku not in prices:
+            # invalid input
+            return -1
+        else:
+            total_cost += prices[sku]["price"]
+
+    return total_cost
+
 
 
