9127709b545c26f1ee1c6fdee725bb8fe0c287c7
sbx_user1059 <sbx_user1059@ip-10-129-102-97.eu-west-2.compute.internal>
2019-01-22 14:11:47 +0000

    Tue Jan 22 14:11:47 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,12 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    """
+    Returns total cost of all items listed in `skus`
+    Args:
+        skus (string) - the SKUs of all the products in the basket
+    Returns:
+        Integer representing the total checkout value of the items 
+    """
+    
+
--- test/solution_tests/HLO/test_hlo.py
+++ test/solution_tests/HLO/test_hlo.py
@@ -4,6 +4,10 @@ from solutions.CHK import checkout_solution
 
 
 class TestCheckout(unittest.TestCase):
-    def test_checkout(self):
-        assert checkout_solution.checkout() == None
+    def test_checkout_simple(self):
+        self.assertEqual(checkout_solution("A"), 50)
+        self.assertEqual(checkout_solution("B"), 30)
+        self.assertEqual(checkout_solution("C"), 20)
+        self.assertEqual(checkout_solution("D"), 15)
+
 
