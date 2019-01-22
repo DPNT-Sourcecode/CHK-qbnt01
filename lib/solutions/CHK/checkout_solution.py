b62d1fde4e080d7923fb8af0e1cfa3812b9aa475
sbx_user1059 <sbx_user1059@ip-10-129-102-97.eu-west-2.compute.internal>
2019-01-22 14:17:47 +0000

    Tue Jan 22 14:17:47 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,8 +1,23 @@
+import csv
+
+
+
 def load_prices():
     """
     Returns list of tuples of information in prices.csv
-    Format:
-        [(SKU, price, deal)]
+    Format: [(SKU, price, deal)]
+    Where `deal` can be null
+    """
+    with open('prices.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        else:
+            print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
+            line_count += 1
+    
 
 # noinspection PyUnusedLocal
 # skus = unicode string
@@ -15,5 +30,6 @@ def checkout(skus):
         Integer representing the total checkout value of the items 
     """
     
+
 
 
