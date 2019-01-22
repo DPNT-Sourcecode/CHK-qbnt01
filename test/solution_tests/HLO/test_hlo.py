1ba6a05770b618286a796fa1a3617f2d265e9a33
sbx_user1059 <sbx_user1059@ip-10-129-102-97.eu-west-2.compute.internal>
2019-01-22 14:08:47 +0000

    Tue Jan 22 14:08:47 UTC 2019

--- test/solution_tests/HLO/test_hlo.py
+++ test/solution_tests/HLO/test_hlo.py
@@ -1,9 +1,9 @@
-from solutions.HLO import hello_solution
+import unittest
+
+from solutions.CHK import checkout_solution
 
 
-class TestHello():
-    def test_hello(self):
-        assert hello_solution.hello("Bob") == "Hello, Bob!"
+class TestCheckout(unittest.TestCase):
+    def test_checkout(self):
+        assert checkout_solution.checkout() == None
 
-    def test_hello_null(self):
-        assert hello_solution.hello("") == "Hello, World!"
